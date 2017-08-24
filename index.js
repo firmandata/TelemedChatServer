1802e51300fcf3b3357868d0dad254ce53d5d03e
firmandata <firman.data@gmail.com>
2017-08-25 06:36:18 +0700

    standard chatting

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ var app = http.createServer(optionServer, function(req, res) {
 	fileServer.serve(req, res);
 }).listen(8080);
 
+var room_general = 'general';
+var docters = [];
+var patients = [];
+
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
 
@@ -24,12 +28,151 @@ io.sockets.on('connection', function(socket) {
 		array.push.apply(array, arguments);
 		socket.emit('log', array);
 	}
-
+	
+	function disconnect() {
+		var socket_id = socket.id;
+		
+		// remove patient if exist
+		var patient = get_patient_from_socket_id(socket_id);
+		if (patient !== null)
+		{
+			// send to all clients
+			socket.broadcast.emit('delete_registered_patient', patients[patient.id]);
+			
+			// delete on patients array
+			delete patients[patient.id];
+		}
+		
+		// remove docter if exist
+		var docter = get_docter_from_socket_id(socket_id);
+		if (docter !== null)
+		{
+			// send to all clients
+			socket.broadcast.emit('delete_registered_docter', docters[docter.id]);
+			
+			// delete on docters array
+			delete docters[docter.id];
+		}
+	}
+	
+	function get_patient_from_socket_id(socket_id)
+	{
+		// get patient by socket_id
+		for (var patient_id in patients)
+		{
+			if (patients[patient_id].socket_id == socket_id)
+				return patients[patient_id];
+		}
+		
+		return null;
+	}
+	
+	function get_docter_from_socket_id(socket_id)
+	{
+		// get docter by socket_id
+		for (var docter_id in docters)
+		{
+			if (docters[docter_id].socket_id == socket_id)
+				return docters[docter_id];
+		}
+		
+		return null;
+	}
+	
+	// { id : docter_id, name : docter_name, photo : url }
+	socket.on('register_docter', function(data) {
+		// joint to general room
+		//socket.join(room_general);
+		
+		// add to docters array
+		docters[data.id] = {
+			id : data.id,
+			socket_id : socket.id,
+			name : data.name,
+			photo : data.photo
+		};
+		
+		// send to client
+		socket.emit('registered_docter', docters[data.id]);
+		
+		// send to all clients
+		socket.broadcast.emit('new_registered_docter', docters[data.id]);
+	});
+	
+	// { id : patient_id, name : patient_name, photo : url }
+	socket.on('register_patient', function(data) {
+		// joint to general room
+		//socket.join(room_general);
+		
+		// add to patients array
+		patients[data.id] = {
+			id : data.id,
+			socket_id : socket.id,
+			name : data.name,
+			photo : data.photo
+		};
+		
+		// send to client
+		socket.emit('registered_patient', patients[data.id]);
+		
+		// send to all clients
+		socket.broadcast.emit('new_registered_patient', patients[data.id]);
+	});
+	
+	socket.on('get_registered_docters', function(){
+		socket.emit('registered_docters', docters);
+	});
+	
+	socket.on('get_registered_patients', function(){
+		socket.emit('registered_patients', patients);
+	});
+	
+	socket.on('get_registered_all', function(){
+		socket.emit('registered_docters', docters);
+		socket.emit('registered_patients', patients);
+	});
+	
+	socket.on('unregister', function(){
+		disconnect();
+	});
+	
+	// default function from socketIO
+	socket.on('disconnect', function(){
+		disconnect();
+	});
+	
+	// { to : to_id, message : message }
+	socket.on('message_send', function(data){
+		var socket_id = socket.id;
+		
+		var user_from = null;
+		user_from = get_patient_from_socket_id(socket_id);
+		if (user_from === null)
+			user_from = get_docter_from_socket_id(socket_id);
+		
+		// find socket_id by id
+		var user_to = null;
+		if (patients[data.to])
+			user_to = patients[data.to];
+		else if (docters[data.to])
+			user_to = docters[data.to];
+		
+		// send to client
+		if (user_to !== null)
+			socket.to(user_to.socket_id).emit('message_receive', {
+				'from'		: user_from,
+				'message'	: data.message
+			});
+	});
+	
+	
 	socket.on('message', function(message) {
 		log('Client said: ', message);
 		// for a real app, would be room-only (not broadcast)
 		socket.broadcast.emit('message', message);
 	});
+	
+	
 
 	socket.on('create or join', function(room) {
 		log('Received request to create or join room ' + room);
