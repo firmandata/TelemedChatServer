41aaa599a0cc9de3968183d64fa068d4fa9f12a9
firmandata <firman.data@gmail.com>
2017-08-27 02:28:24 +0700

    Finish normal signaling

--- index3.js
+++ index3.js
@@ -35,7 +35,7 @@ io.sockets.on('connection', function(socket) {
 		if (user_key !== null)
 		{
 			// send to all clients
-			socket.broadcast.emit('user_other_unregistered', users[user_key]);
+			socket.broadcast.emit('delete_registered', users[user_key]);
 			
 			// delete on users array
 			delete users[user_key];
@@ -65,18 +65,27 @@ io.sockets.on('connection', function(socket) {
 			socket_id	: socket.id,
 			type		: data.type,
 			name		: data.name,
-			photo		: data.photo,
-			oncall		: false
+			photo		: data.photo
 		};
 		
 		// add to users array
 		users[key] = user;
 		
 		// send to user
-		socket.emit('user_registered', user);
+		socket.emit('registered', user);
 		
 		// send to all users
-		socket.broadcast.emit('user_other_registered', user);
+		socket.broadcast.emit('registered_new', user);
+	});
+	
+	socket.on('get_registered_all', function() {
+		// remove key from users array using create new user list
+		var user_list = [];
+		for (var user_key in users)
+			user_list.push(users[user_key]);
+		
+		// send to user
+		socket.emit('registered_all', user_list);
 	});
 	
 	socket.on('unregister', function(){
@@ -86,99 +95,6 @@ io.sockets.on('connection', function(socket) {
 	// default function from socketIO
 	socket.on('disconnect', function(){
 		disconnect();
-	});
-	
-	socket.on('get_user_others', function() {
-		var socket_id = socket.id;
-		
-		// remove key from users array using create new user list
-		var user_list = [];
-		for (var user_key in users)
-		{
-			var user = users[user_key];
-			if (user.socket_id != socket_id)
-				user_list.push(user);
-		}
-		
-		// send to user
-		socket.emit('get_user_others', user_list);
-	});
-	
-	socket.on('dial', function(data) {
-		var socket_id = socket.id;
-		
-		var user_from = null;
-		var user_to = users[data.to];
-		
-		var user_key_from = get_user_key_by_socket_id(socket_id);
-		if (user_key_from !== null)
-			user_from = users[user_key_from];
-		
-		if (user_to && user_from !== null)
-		{
-			if (data.type == 'dialing')
-			{
-				if (!user_to.oncall)
-				{
-					// set to user as oncall
-					user_from.oncall = true;
-					user_to.oncall = true;
-					
-					// send to user
-					socket.emit('user_other_oncall', user_to);
-				
-					// send to user target
-					socket.to(user_to.socket_id).emit('dial_' + data.type, user_from);
-					
-					// send to all users
-					for (var user_key in users)
-					{
-						var user = users[user_key];
-						
-						if (user_from.key != user_key)
-							socket.to(user.socket_id).emit('user_other_oncall', user_from);
-						if (user_to.key != user_key)
-							socket.to(user.socket_id).emit('user_other_oncall', user_to);
-					}
-				}
-				else
-				{
-					// send to user
-					socket.emit('dial_' + data.type + '_failed', {
-						to		: user_to,
-						message	: "User already on call right now."
-					});
-				}
-			}
-			else if (data.type == 'answer')
-			{
-				// send to user target
-				socket.to(user_to.socket_id).emit('dial_' + data.type, user_from);
-			}
-			else if (data.type == 'reject' || data.type == 'end')
-			{
-				// set to user as offcall
-				user_from.oncall = false;
-				user_to.oncall = false;
-				
-				// send to user
-				socket.emit('user_other_offcall', user_to);
-				
-				// send to user target
-				socket.to(user_to.socket_id).emit('dial_' + data.type, user_from);
-				
-				// send to all users
-				for (var user_key in users)
-				{
-					var user = users[user_key];
-					
-					if (user_from.key != user_key)
-						socket.to(user.socket_id).emit('user_other_offcall', user_from);
-					if (user_to.key != user_key)
-						socket.to(user.socket_id).emit('user_other_offcall', user_to);
-				}
-			}
-		}
 	});
 	
 	// { to : key, message : message }
@@ -193,13 +109,11 @@ io.sockets.on('connection', function(socket) {
 			user_from = users[user_key_from];
 		
 		// send to user to
-		if (user_to)
-		{
+		if (user_to !== null)
 			socket.to(user_to.socket_id).emit('message_receive', {
 				'from'		: user_from,
 				'message'	: data.message
 			});
-		}
 	});
 	
 	
